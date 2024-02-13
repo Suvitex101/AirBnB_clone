42c5ce5beb5a3c95809b0a983fccdce435d15587
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2024-02-13 18:18:33 +0000

    test case for base model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,43 @@
+import unittest
+from models import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        # Test __init__() method
+        model = BaseModel()
+        self.assertTrue(hasattr(model, 'id'))
+        self.assertTrue(hasattr(model, 'created_at'))
+        self.assertTrue(hasattr(model, 'updated_at'))
+        self.assertIsInstance(model.id, str)
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_save(self):
+        # Test save() method
+        model = BaseModel()
+        old_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(old_updated_at, model.updated_at)
+
+    def test_to_dict(self):
+        # Test to_dict() method
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertIn('__class__', model_dict)
+        self.assertIn('id', model_dict)
+        self.assertIn('created_at', model_dict)
+        self.assertIn('updated_at', model_dict)
+
+    def test_str(self):
+        # Test __str__() method
+        model = BaseModel()
+        string_representation = model.__str__()
+        self.assertIsInstance(string_representation, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
